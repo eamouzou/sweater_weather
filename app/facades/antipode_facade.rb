3209927a7ff31c2f6c3f2bd05e3b7f81481d6d77
Elom Amouzou <elom1990@yahoo.com>
2020-04-20 10:31:07 -0600

    add antipode forecast helper method

--- app/facades/antipode_facade.rb
+++ app/facades/antipode_facade.rb
@@ -1,17 +1,22 @@
 class AntipodeFacade
   def initialize(search_location)
     @search_location = search_location
-    @get = antipode_coordinates
+    @get = antipode_forecast
   end
 
   private
 
   def search_coordinates
-      coordinates ||= CoordinateService.new(@search_location)
+    coordinates ||= CoordinateService.new(@search_location)
   end
 
   def antipode_coordinates
     coordinates ||= AntipodeService.new(search_coordinates.latitude, search_coordinates.longitude)
+  end
+
+  def antipode_forecast
+    weather_info = WeatherDataService.new(antipode_coordinates.latitude, antipode_coordinates.longitude).data
+    antipode_forecast = Forecast.new(weather_info)
     require "pry"; binding.pry
   end
 

284552329c31bfbc8a441680b5b8858f9c314a8c
Elom Amouzou <elom1990@yahoo.com>
2020-04-20 11:09:42 -0600

    refactor show action in antipodes controller add initialize for antipode

--- app/controllers/api/v1/antipodes_controller.rb
+++ app/controllers/api/v1/antipodes_controller.rb
@@ -1,8 +1,8 @@
 class Api::V1::AntipodesController < ApplicationController
 
   def show
-    antipode_info = AntipodeFacade.new(params[:location])
-    render json: AntipodeSerializer.new(antipode_info)
+    antipode_info = AntipodeFacade.new(params[:location]).antipode_info
+    render json: AntipodeSerializer.new(Antipode.new(antipode_info))
   end
 
 end

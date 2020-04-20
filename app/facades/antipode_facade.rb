c21d6e849f1bd072df16cae6f639e12210603038
Elom Amouzou <elom1990@yahoo.com>
2020-04-20 10:45:27 -0600

    add get location name helper method

--- app/facades/antipode_facade.rb
+++ app/facades/antipode_facade.rb
@@ -1,7 +1,11 @@
 class AntipodeFacade
   def initialize(search_location)
     @search_location = search_location
-    @get = antipode_forecast
+    @get = get_location_name
+  end
+
+  def antipode
+    require "pry"; binding.pry
   end
 
   private
@@ -17,7 +21,11 @@ class AntipodeFacade
   def antipode_forecast
     weather_info = WeatherDataService.new(antipode_coordinates.latitude, antipode_coordinates.longitude).data
     antipode_forecast = Forecast.new(weather_info)
-    require "pry"; binding.pry
+  end
+
+  def get_location_name
+    lat_long = "#{antipode_coordinates.latitude}, #{antipode_coordinates.longitude}"
+    search_coordinates.location_name(lat_long)
   end
 
 end

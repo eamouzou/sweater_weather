2aaa30539b21f641d05bdbae2904e0b3afa53248
Elom Amouzou <elom1990@yahoo.com>
2020-04-20 11:08:55 -0600

    revise antipode request test

--- spec/requests/api/v1/antipode_request_spec.rb
+++ spec/requests/api/v1/antipode_request_spec.rb
@@ -4,16 +4,16 @@ RSpec.describe 'Antipode API' do
   it "gets antipode attributes from location" do
     get '/api/v1/antipode?location=hongkong'
 
-    expect(response).to be be_successful
+    expect(response).to be_successful
 
     antipode_response = JSON.parse(response.body, symbolize_names: true)
 
     antipode_attributes = antipode_response[:data][:attributes]
-    expected_attr_keys = ["location_name", "forecast", "current_temperature", "search_location"]
+    expected_attr_keys = [:location_name, :forecast, :search_location]
 
-    expect(antipode_response[:data][:id]).to eq('null')
+    expect(antipode_response[:data][:id]).to be_truthy
     expect(antipode_response[:data][:type]).to eq('antipode')
     expect(antipode_attributes.keys).to eq(expected_attr_keys)
-    expect(antipode_attributes[:search_location]).to eq('hongkong')
+    expect(antipode_attributes[:search_location]).to eq('Hong Kong')
   end
 end

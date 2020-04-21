73946b920d1e9b189856a4103e4b16e748c32fc0
Elom Amouzou <elom1990@yahoo.com>
2020-04-20 23:30:15 -0600

    add user validations and security checks; tests pass

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  validates :email, presence: true, uniqueness: true
+  validates_presence_of :password_digest, require: true
+  has_secure_password
+  has_secure_token :api_key
 end

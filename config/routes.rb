c1b3d89417a43066a1df123c2d4f7332727b4936
Elom Amouzou <elom1990@yahoo.com>
2020-04-21 01:06:36 -0600

    add login route

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
       get '/backgrounds', to: 'backgrounds#index'
       get '/antipode', to: 'antipodes#show'
       post '/users', to: 'users#create'
+      post '/sessions', to: 'sessions#create'
     end
   end
 end

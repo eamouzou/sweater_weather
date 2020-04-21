47fccc55233030f1daf1638ae7c995811aaf0bc4
Elom Amouzou <elom1990@yahoo.com>
2020-04-21 00:32:57 -0600

    revise registration test

--- spec/requests/api/v1/registration_request_spec.rb
+++ spec/requests/api/v1/registration_request_spec.rb
@@ -15,15 +15,15 @@ RSpec.describe 'User Registration' do
     user = User.last
     expected_keys = ['email', 'api_key']
 
-    expect(user_data['data']['type']).to eq("users")
+    expect(user_data['data']['type']).to eq("user")
     expect(user_data['data']['attributes'].keys).to eq(expected_keys)
     expect(user_data['data']['attributes']['api_key']).to eq(user.api_key)
   end
 
-  it 'returns a 400 level status code and body with explanatory description' do
+  it 'returns a 400 level status code and fail message if email is already used' do
     User.create(email: 'badexample@yoyo.com', password: 'no', password_confirmation: 'no')
 
-    params = {email: "yeayea@ok.com",
+    params = {email: "badexample@yoyo.com",
       password: "password",
       password_confirmation: "password"}
 
@@ -33,6 +33,35 @@ RSpec.describe 'User Registration' do
 
     user_data = JSON.parse(response.body)
 
+    expect(user_data['failed']).to eq("Email has already been taken")
+  end
+
+  it 'returns a 400 level status code and fail message if passwords do not match' do
+    params = {email: "badexample@yoyo.com",
+      password: "no",
+      password_confirmation: "password"}
+
+    post '/api/v1/users', params: params
+
+    expect(response.status).to eq(400)
+
+    user_data = JSON.parse(response.body)
+
+    expect(user_data['failed']).to eq("Password confirmation doesn't match Password")
+  end
+
+  it 'returns a 400 level status code and fail message if field is missing' do
+    params = {email: "",
+      password: "",
+      password_confirmation: "password"}
+
+    post '/api/v1/users', params: params
+
+    expect(response.status).to eq(400)
+
+    user_data = JSON.parse(response.body)
+
+    expect(user_data['failed']).to eq("Email can't be blank, Password digest can't be blank, and Password can't be blank")
   end
 
 end

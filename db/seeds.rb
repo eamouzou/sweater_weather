21ca6063cf6645b72f3192c974483d6cbb95bcd7
Elom Amouzou <elom1990@yahoo.com>
2020-04-21 14:49:28 -0600

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(email: "yeayea@ok.com", password: "password",
+  password_confirmation: "password", api_key: "jgn983hy48thw9begh98h4539h4")
+
+User.create(email: 'badexample@yoyo.com', password: 'no', password_confirmation: 'no')
+
+User.create(email: 'example@yoyo.com', password: 'yes', password_confirmation: 'yes')

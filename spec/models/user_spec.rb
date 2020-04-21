e2e2ca740cad7cb46a5294a55ad4cc579b7a35a8
Elom Amouzou <elom1990@yahoo.com>
2020-04-20 23:29:55 -0600

    add user model test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { should validate_presence_of :email}
+    it { should validate_uniqueness_of :email}
+    it { should validate_presence_of :password}
+    it { should have_secure_password }
+    it { should validate_presence_of :password_digest}
+    it { should have_secure_token :api_key}
+  end
+
+  describe 'user creation' do
+    it 'is created with valid parameters' do
+      user = User.new(email: 'hello@works.com', password: 'password', password_confirmation: 'password')
+
+      expect(user.save).to eq(true)
+    end
+
+    it 'is not created with invalid parameters' do
+      user = User.new(email: 'hello@works.com', password: '', password_confirmation: 'password')
+
+      expect(user.save).to eq(false)
+    end
+  end
 end
